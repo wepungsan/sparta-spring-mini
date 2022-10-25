396fcc7f485ee8f77b2ccedb765561ee9a7d9aa1
wepungsan <wepungsan@gmail.com>
2022-10-25 20:00:43 +0900

    AccessToken Reissue

--- src/main/java/com/sparta/homework2/jwt/JwtFilter.java
+++ src/main/java/com/sparta/homework2/jwt/JwtFilter.java
@@ -1,5 +1,9 @@
 package com.sparta.homework2.jwt;
 
+import com.sparta.homework2.dto.TokenDto;
+import com.sparta.homework2.model.RefreshToken;
+import com.sparta.homework2.repository.RefreshTokenRepository;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -18,8 +22,8 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String BEARER_PREFIX = "Bearer ";
-
     private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
@@ -29,9 +33,34 @@ public class JwtFilter extends OncePerRequestFilter {
 
         // 2. validateToken 으로 토큰 유효성 검사
         // accessToken 검사
-        if(StringUtils.hasText(accessToken) && tokenProvider.validateToken(accessToken)) {
+        try {
+            if(StringUtils.hasText(accessToken) && tokenProvider.validateToken(accessToken)) {
+                Authentication authentication = tokenProvider.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (ExpiredJwtException e) {
+            // 1. Refresh Token 검증
+            if (!tokenProvider.validateToken(refreshToken)) {
+                throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
+            }
+
+            // 2. Access Token 에서 Member ID 가져오기
             Authentication authentication = tokenProvider.getAuthentication(accessToken);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+            RefreshToken refreshTokenObj = refreshTokenRepository.findByKey(authentication.getName())
+                    .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
+
+            // 4. Refresh Token 일치하는지 검사
+            if (!refreshTokenObj.getValue().equals(refreshToken)) {
+                throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+            }
+
+            // 5. 새로운 토큰 생성
+            TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+            // 6. 토큰 발급
+            response.setHeader("AccessToken", tokenDto.getAccessToken());
         }
 
         // refreshToken 검사
